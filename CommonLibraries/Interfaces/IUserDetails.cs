c8cdb259f4fcba10ad7a57655676338d3b914764
Tutaram Rasi <tutaram.rasi@audreeinfotech.com>
2021-08-23 16:43:01 +0530

    Add project files.

---
+++ CommonLibraries/Interfaces/IUserDetails.cs
@@ -0,0 +1,19 @@
+﻿using Products.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibraries.Interfaces
+{
+    public interface IUserDetails
+    {
+        Task<dynamic> GetAllUserDetails();
+        Task<dynamic> GetUserDetailsById(long id);
+        Task<dynamic> SaveUserDetails(UserDetails userDetails);
+        Task<dynamic> UpdateUserDetails(UserDetails userDetails);
+        Task<dynamic> DeleteUserDetails(UserDetails userDetails);
+
+    }
+
+}
---
+++ DataManager/Manager/UserDetailsManager.cs
@@ -0,0 +1,48 @@
+﻿using CommonLibraries.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Products.DBContext;
+using Products.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataManager.Manager
+{
+    public class UserDetailsManager : IUserDetails
+    {
+        private readonly IDataUoW _dataUow;
+        public UserDetailsManager(IDataUoW dataUow)
+        {
+            _dataUow = dataUow;
+        }
+        public async Task<dynamic> GetAllUserDetails()
+        {
+            var userDetailsData = await _dataUow.UserDetails.GetAll().ToListAsync();
+            return userDetailsData;
+        }
+        public async Task<dynamic> DeleteUserDetails(UserDetails userDetails)
+        {
+            _dataUow.UserDetails.Delete(userDetails);
+            await _dataUow.CommitAsync("");
+            return "Success";
+        }
+        public async Task<dynamic> UpdateUserDetails(UserDetails userDetails)
+        {
+            _dataUow.UserDetails.Update(userDetails);
+            await _dataUow.CommitAsync("");
+            return "Success";
+        }
+        public async Task<dynamic> SaveUserDetails(UserDetails userDetails)
+        {
+            _dataUow.UserDetails.Add(userDetails);
+            await _dataUow.CommitAsync("");
+            return "Success";
+        }
+        public async Task<dynamic> GetUserDetailsById(long id)
+        {
+            var userDetailsData = await _dataUow.UserDetails.GetAllWithNoTracking().Where(p => p.UserDetailsId == id).FirstOrDefaultAsync();
+            return userDetailsData;
+        }
+
+
+    }
+}
